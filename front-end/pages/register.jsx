5295284f21bfb79c180cf503399d400573fb0a3e
mouadHILLALI <mauadtoto001@gmail.com>
2024-04-02 14:33:02 +0000

    linking back with front

--- front-end/pages/register.jsx
+++ front-end/pages/register.jsx
@@ -1,10 +1,43 @@
+import { useState } from "react";
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const submitForm = (e) =>{
+    e.preventDefault();
+  }
   return (
-    <div className="bg-[#fae6cf] h-[100vh] ">
-      <form className=" w-[50%] flex flex-col m-auto">
-        <input type="text" name="name" />
-        <input type="email" name="email" />
-        <input type="password" name="password" />
+    <div className="bg-[#fae6cf] h-[100vh] flex ">
+      <form onSubmit={submitForm} className=" w-[30%] h-[40%] flex flex-col m-auto justify-between">
+        <input
+          type="text"
+          name="name"
+          className=" p-3 rounded-[15px]"
+          placeholder="Enter your name:"
+          value={name}
+          onChange={(e)=>{setName(e.target.value)}}
+        />
+        <input
+          type="email"
+          name="email"
+          className=" p-3 rounded-[15px]"
+          placeholder="Enter your email:"
+          value={email}
+          onChange={(e)=>{setEmail(e.target.value)}}
+        />
+        <input
+          type="password"
+          name="password"
+          className=" p-3 rounded-[15px]"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e)=>{setPassword(e.target.value)}}
+        />
+        <div className="w-full">
+            <button className="w-full p-2 bg-[#464553] rounded-lg text-white font-bold ">
+                Register
+            </button>
+        </div>
       </form>
     </div>
   );
