0bedb4a99ebd4f6958a7b744968b5b470e7fbd23
mouadHILLALI <mauadtoto001@gmail.com>
2024-04-05 03:02:46 +0000

    delete trajectory

--- front-end/components/trajectories.jsx
+++ front-end/components/trajectories.jsx
@@ -2,10 +2,10 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 const Trajectory = () => {
+  const token = localStorage.getItem("token");
   const [trajectories, setTrajectories] = useState([]);
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
     const fetchTrajectories = async () => {
       try {
         const res = await axios.get("http://localhost/api/Trajectory/Show", {
@@ -21,6 +21,26 @@ const Trajectory = () => {
     fetchTrajectories();
   }, []);
 
+  const deleteTrajectory = async (id) => {
+    try {
+      const res = await axios.delete(
+        `http://localhost/api/Trajectory/Delete/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(res);
+      setTrajectories(
+        trajectories.filter((trajectory) => trajectory.id !== id)
+      );
+      fetchTrajectories();
+    } catch (error) {
+      console.error("Error deleting trajectory:", error);
+    }
+  };
+
   return (
     <>
       <div className="w-[80%] h-[30%] border-2 border-black rounded-[15px] m-auto mt-7 ">
@@ -33,6 +53,14 @@ const Trajectory = () => {
             <img src={trajectory.image} alt="..." />
             <h3>Start date: {trajectory.start_date}</h3>
             <h3>End date: {trajectory.end_date}</h3>
+            <div className="flex items-center w-full ">
+              <button
+                onClick={() => deleteTrajectory(trajectory.id)}
+                className="font-bold p-2 bg-red-500 rounded-[15px] border-2 border-black"
+              >
+                Delete
+              </button>
+            </div>
           </div>
         ))}
       </div>
